cd0cfd11c1a30e5541a1577436250a489512282f
iri-rsl <iri.rasolo@gmail.com>
2023-05-15 14:13:20 +0200

    réglage des bounds

--- src/sprites/RockPlat.py
+++ src/sprites/RockPlat.py
@@ -7,10 +7,10 @@ import pygame
 class MyRockPlatform(pygame.sprite.Sprite):
 	def __init__(self, window_width, rock_list_left, rock_list_right):
 		super().__init__()
-		self.side = randint(0, 1)
-		bounds = [(0, window_width//2), (window_width//2,window_width)]
 		self.image = pygame.image.load('assets/images/rock_platforms/1.png')
 		self.rect = self.image.get_rect()
+		self.side = randint(0, 1)
+		bounds = [(0, window_width//2), (window_width//2,window_width-self.rect.width)]
 		self.rect.x = randint(bounds[self.side][0],bounds[self.side][1])
 		if not(self.side) and not(rock_list_left) or self.side and not(rock_list_right) : 
 			self.rect.y = 520
