330293fd9f2e4391221e75767dca4d073f1865dd
hickatheworld <cc@hicka.world>
2023-05-15 19:26:49 +0200

    fucking sound

--- src/sprites/Sound.py
+++ src/sprites/Sound.py
@@ -2,14 +2,13 @@ import pygame
 from random import randint
 
 class Sound():
-    def __init__(self): 
-        self.music= pygame.mixer_music.load(f"assets/sounds/game_music/0.mp3")
-        self.music_volume=pygame.mixer.music.set_volume(0.25)
-        self.music_play=pygame.mixer.music.play(loops=-1)
-        self.footstep_sound=pygame.mixer.Sound("assets/sounds/footstep.wav")
-        #self.footstep_sound_play=pygame.mixer.music.play(loops=-1)
-        self.jump1=pygame.mixer.Sound("assets/sounds/jump_01.wav")
-        self.jump2=pygame.mixer.Sound("assets/sounds/jump_02.wav")
+    def __init__(self):
+        pygame.mixer.music.load(f'assets/music/ingame.mp3')
+        pygame.mixer.music.set_volume(0.25)
+        pygame.mixer.music.play(loops=-1)
+        self.footstep_sound = pygame.mixer.Sound('assets/sfx/footstep.wav')
+        self.jump1 = pygame.mixer.Sound('assets/sfx/jump_01.wav')
+        self.jump2 = pygame.mixer.Sound('assets/sfx/jump_02.wav')
         self.sound_volume()
         
     def sound_volume(self):
